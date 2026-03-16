90950f19d4075016848d67ed48762c3856a55dd3
Nuttayos Suvisuttikasame <Nuttayos.Suv@AIGEN-Nuttayos-Suv.local>
2026-03-16 16:26:23 +0700

    chore: register admin signup & admin job

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,27 +1,37 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
-import { AuthController } from './auth.controller.js';
+import { ZitadelService } from '../zitadel/zitadel.service.js';
 import { AdminController } from './admin.controller.js';
 import { AdminService } from './admin.service.js';
-import { GatewayController } from './gateway.controller.js';
+import { AdminSignupProcessor } from './admin-signup.processor.js';
+import { AdminSignupService } from './admin-signup.service.js';
+import { ADMIN_SIGNUP_QUEUE } from './admin-signup.queue.js';
+import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
+import { GatewayController } from './gateway.controller.js';
 import { OidcClientService } from './oidc-client.service.js';
 import { RootGuard } from './root.guard.js';
 import { SessionGuard } from './session.guard.js';
 import { SessionService } from './session.service.js';
 import { UserProvisionProcessor } from './user-provision.processor.js';
 import { USER_PROVISION_QUEUE } from './user-provision.queue.js';
-import { ZitadelService } from '../zitadel/zitadel.service.js';
 
 @Module({
   imports: [
-    BullModule.registerQueue({
-      name: USER_PROVISION_QUEUE,
-    }),
+    BullModule.registerQueue(
+      {
+        name: USER_PROVISION_QUEUE,
+      },
+      {
+        name: ADMIN_SIGNUP_QUEUE,
+      },
+    ),
   ],
   controllers: [AuthController, AdminController, GatewayController],
   providers: [
     AdminService,
+    AdminSignupProcessor,
+    AdminSignupService,
     AuthService,
     OidcClientService,
     RootGuard,
